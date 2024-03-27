c170f9796e64e2e0df836ae612c9de119072580a
Sashakirina <111364619+Sashakirina@users.noreply.github.com>
2024-03-27 10:37:18 +0000

    Deploying to gh-pages from @ Sashakirina/goit-js-hw-09@b6ff7e1262b9b0a1bf4311bf92acfef6775a7f8c 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const o=document.querySelector(".feedback-form");o.addEventListener("input",r);function r(t){console.log(t.target.value);const e=[t.target.value.trim()];localStorage.setItem("feedback-form-state",JSON.stringify(e))}
+//# sourceMappingURL=commonHelpers2.js.map
